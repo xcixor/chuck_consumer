b485b89b624fd38922d642647d34d9245304f955
xcixor <pndungu54@gmail.com>
2022-01-06 19:50:05 +0300

    style(landing): improve the appearance of jokes

--- app/consumer/static/home/js/home.js
+++ app/consumer/static/home/js/home.js
@@ -1,31 +1,46 @@
 function getContent(category){
-  var url = `/${category}/`;
+    if ( $(`#${category}-contentContainer`).children().length > 0 ) {
+       return;
+    }
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    var url = `/${category}/`;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Accept', 'application/json');
+
+
+    let container = $(`#${category}-contentContainer`).parent();
+    container.addClass('loading');
+    container.find('.fa-circle-notch').toggleClass('hidden');
+
+    xhr.addEventListener('readystatechange', function(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
         var response = JSON.parse(xhr.response);
         createCategoryItems(category, response);
-    }else {
-        var errors = xhr.status;
-        console.error(errors)
-    }
-    };
-  xhr.send();
+
+        let container = $(`#${category}-contentContainer`).parent();
+        container.find('.fa-circle-notch').toggleClass('hidden');
+        container.removeClass('loading');
+        }
+        else {
+            console.error('errors')
+        }
+    });
+    xhr.send();
 }
 
 var createCategoryItems = function(category, data){
     let contentContainerContainer = $(`#${category}-contentContainer`);
+    contentContainerContainer.removeClass('hidden');
     data.result.forEach(function (item, index) {
-        console.log(item)
         let itemContainer = $('<div>').addClass('item-container flex');
         let rightContainer = $('<div>').addClass('right-container');
         let leftContainer = $('<div>').addClass('left-container');
         let img = $('<img/>').attr('src', item.icon_url);
-        let head = $('<p/>').html(`item: ${item.id}`);
+        let position = index + 1;
+        let head = $('<p/>').html(`${position}: ${item.value}`);
         rightContainer.append(head);
         leftContainer.append(img);
         itemContainer.append(leftContainer);
@@ -34,6 +49,11 @@ var createCategoryItems = function(category, data){
     });
 }
 
-$('.category-card').on('click', function(){
-    $('.category-content').toggleClass('hidden');
-})
+var closeJokesContainer = function(event, category){
+    if ( $(`#${category}-contentContainer`).children().length > 0 ) {
+        let contentContainerContainer = $(`#${category}-contentContainer`);
+        contentContainerContainer.toggleClass('hidden');
+        $(event.target).toggleClass('fa-angle-down fa-angle-up');
+    }
+
+}
